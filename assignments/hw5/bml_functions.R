a6bbc627683ae1408a35c0834ec7b3d0381f9b2a
Caren Thomas <carenthomas@gmail.com>
2015-03-16 09:51:12 -0700

    Final bml_functions.R

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -8,9 +8,8 @@
 ## that stores the state of the system (i.e. location of red and blue cars)
 
 bml.init <- function(r, c, p){
-
-  
-   #return(m)
+  m <- matrix(sample(0:2, r*c, replace=TRUE, prob=c(1 - p, p/2, p/2)), nrow=r, ncol=c)  
+  return(m)
 }
 
 #### Function to move the system one step (east and north)
@@ -21,10 +20,48 @@ bml.init <- function(r, c, p){
 ## NOTE : the function should move the red cars once and the blue cars once,
 ## you can write extra functions that do just a step north or just a step east.
 
-bml.step <- function(m){
-
+bml.step <- function(m){  
   
-   #return(list(m, grid.new))
+  grid.new <- FALSE
+  
+  m.check <- cbind(m[,2:ncol(m)], m[,1])
+  m.final <- m
+  
+  for (red_r in 1:nrow(m)) {
+    for (red_c in 1:ncol(m)) {
+      if (m[red_r, red_c] == 1) {
+        if (m.check[red_r, red_c] == 0) {
+          m.final[red_r, red_c] <- 0
+          if (red_c == ncol(m)) {
+            m.final[red_r, 1] <- 1
+          } else {
+            m.final[red_r, red_c + 1] <- 1
+          }
+          grid.new <- TRUE
+        }
+      }
+    }
+  }
+  
+  m.check <-rbind(m.final[nrow(m),], m.final[1:(nrow(m)-1),])
+  
+  for (blue_r in 1:nrow(m)) {
+    for (blue_c in 1:ncol(m)) {
+      if (m[blue_r, blue_c] == 2) {
+        if (m.check[blue_r, blue_c] == 0) {
+          m.final[blue_r, blue_c] <- 0
+          if (blue_r == 1) {
+            m.final[nrow(m), blue_c] <- 2
+          } else {
+            m.final[blue_r - 1, blue_c] <- 2
+          }
+          grid.new <- TRUE
+        }
+      }
+    }
+  }
+  
+  return(list(m.final, grid.new))
 }
 
 #### Function to do a simulation for a given set of input parameters
@@ -32,5 +69,16 @@ bml.step <- function(m){
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
 bml.sim <- function(r, c, p){
-
+  m <- bml.init(r, c, p)
+  count <- 0
+  for (i in 1:5000) {
+    b <- bml.step(m)
+    m <- b[[1]]
+    if (b[[2]]) {
+      count <- count + 1
+    } else {
+      return(list(m, count))
+    }
+  }
+  return(list(m, count))
 }
